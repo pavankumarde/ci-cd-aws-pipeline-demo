e99928b16c2c2d2c1de344f778aa2187bd8ad8bd
pavankumarde <128978077+pavankumarde@users.noreply.github.com>
2023-03-26 20:41:28 +0530

    Add files via upload

---
+++ ci-cd-aws-pipeline-demo.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CiCdAwsPipelineDemoStack } from '../lib/ci-cd-aws-pipeline-demo-stack';
+
+const app = new cdk.App();
+new CiCdAwsPipelineDemoStack(app, 'CiCdAwsPipelineDemoStack', {
+  env: {
+    account: '109244373897',
+    region: 'ap-south-1',
+  }
+});
+
+app.synth();
